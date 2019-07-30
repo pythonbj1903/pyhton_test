9632906423f3b2b2ae2a2d4a92c44483971ac4e6
jiaojian777 <53417219+jiaojian777@users.noreply.github.com>
2019-07-30 12:00:21 +0800

    Develop (#3)
    * secoud commit
    
    * some changes

--- swipertest/swipertest/wsgi.py
+++ swipertest/swipertest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for swiper project.
+WSGI config for swipertest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swiper.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swipertest.settings")
 
 application = get_wsgi_application()
